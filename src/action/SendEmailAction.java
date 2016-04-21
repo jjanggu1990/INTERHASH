b821757e99861d7e5a58aa84336f40c8e611c41b
LeeJinOh <jjanggu1990@naver.com>
2016-04-21 20:33:54 +0900

    이메일보내는 기능 구현

--- src/action/SendEmailAction.java
+++ src/action/SendEmailAction.java
@@ -1,5 +1,13 @@
 package action;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,10 +15,52 @@ public class SendEmailAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		
-		
-		
-		
+		sendmail();
 		return "";
 	}
+
+	final String username = "fksh90@gmail.com";
+	final String password = "q131313!#";
+
+	public void sendmail() {
+
+		Properties props = new Properties();
+		/*
+		 * props.put("mail.smtp.user",username); props.put("mail.smtp.password",
+		 * password);
+		 */
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "25");
+		props.put("mail.debug", "false");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.EnableSSL.enable", "true");
+		props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		System.out.println("??");
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("fksh90@gmail.com"));//
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("jjanggu1990@naver.com"));
+			message.setContent("<a href='http://www.naver.com'>naver</a>","text/html; charset=UTF-8");//글내용을 html타입
+			message.setSubject("Testing Subject");
+			message.setText("<a href='http://www.naver.com'>naver</a> Dear Mail Crawler," + "\n\n No spam to my email, please!");// 내용
+			// charset설정
+			System.out.println("send!!!");
+			Transport.send(message);
+			System.out.println("SEND");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
