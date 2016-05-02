5d0f94d4c42c1b749c856e81fd54e30573a3c732
soonyoung <keymaker0752@gmail.com>
2016-05-02 17:25:14 +0900

    순영컴밋

--- src/action/ContentViewAction.java
+++ src/action/ContentViewAction.java
@@ -11,11 +11,14 @@ import board.CommentDBBean;
 import board.CommentDataBean;
 import board.ContentDBBean;
 import board.ContentDataBean;
+import board.LogonDBBean;
+import board.LogonDataBean;
 
 public class ContentViewAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
 		int connum = 0 ;
 		try{
 		connum = Integer.parseInt(request.getParameter("connum"));
@@ -30,37 +33,11 @@ public class ContentViewAction implements CommandAction {
 		request.setAttribute("sdf", sdf);
 		CommentDBBean commentdbpro = CommentDBBean.getInstance();
 		
-		
-/*		String comnick = (String)request.getSession().getAttribute("nickName");  
-		String comcontent = request.getParameter("comcontent");
-		Timestamp comcreateddate; 
-		Timestamp commodifieddate;
-		String comip = request.getRemoteAddr();
-		String email = (String) request.getSession().getAttribute("memId");
-		
-		
-		CommentDBBean commentdbpro = CommentDBBean.getInstance();
-		CommentDataBean comment = new CommentDataBean();
-		
-		comment.setComnick(comnick);
-		comment.setComcontent(comcontent);	
-		comment.setComcreateddate(new Timestamp(System.currentTimeMillis()));
-		comment.setCommodifieddate(new Timestamp(System.currentTimeMillis()));
-		comment.setComip(comip);
-		comment.setComreportcount(0);
-		comment.setComblind("0");
-		comment.setConnum(connum);
-		comment.setEmail(email);
-		
 
-		commentdbpro.insertComment(comment);
-		*/
 		ArrayList array = commentdbpro.getComment();
-		/*request.setAttribute("comments", comment);*/
+
 		request.setAttribute("comment", array);
 		
-		
-	
 		return "/userpage/ContentView.jsp";
 	}
 
